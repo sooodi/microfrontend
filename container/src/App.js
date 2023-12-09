34f8ac8ed99c63e1f30a716b37f59ede27a2808b
“sooodi” <sudytaher@gmail.com>
2023-12-09 22:51:11 +0330

    navigation

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Switch>
+            {/* <Route path="/auth" component={AuthApp} /> */}
+            <Route path="/" component={MarketingApp} />
+          </Switch>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
